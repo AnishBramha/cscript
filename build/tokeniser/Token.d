e33d09b52019563b900a719dd14404ffa90f03c3
AnishBramha <Anish.Teja@iiitb.ac.in>
2025-11-10 02:04:07 +0530

    Modified .gitignore

--- build/tokeniser/Token.d
+++
@@ -1,7 +0,0 @@
-build/tokeniser/Token.o: tokeniser/Token.cpp tokeniser/./Token.hpp \
-  tokeniser/././TokenType.hpp tokeniser/./../superclass/super.hpp \
-  tokeniser/../superclass/super.hpp
-tokeniser/./Token.hpp:
-tokeniser/././TokenType.hpp:
-tokeniser/./../superclass/super.hpp:
-tokeniser/../superclass/super.hpp:
